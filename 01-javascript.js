aed85bd4673e03193ef0fb70cc2c70196bdd4a76
taniyahaghighi <52607467+taniyahaghighi@users.noreply.github.com>
2019-08-05 12:54:45 +0430

    Add files via upload

---
+++ 01-javascript.js
@@ -0,0 +1,9 @@
+
+var number,anothernumber;
+number=12;
+anothernumber= 5;
+debugger
+number=number+anothernumber;
+console.log(number);
+
+
---
+++ 02-javascript.js
@@ -0,0 +1,5 @@
+var x = 10,
+    y = 2,
+    z;
+z = y * x;
+console.log(z);
